1b3cc7048fe9ba577affa2bc623f22aa48a64717
helauren-laptop <helauren42@gmail.com>
2024-11-11 03:45:32 +0100

    added join strings for containers with iterators and stack and queue

--- Conversions/Conversions.hpp
+++ Conversions/Conversions.hpp
@@ -1,10 +1,31 @@
 #pragma once
 #include "../Printer/Printer.hpp"
 
-std::string vectorToString(const std::vector<std::string> vec, const std::string& sep = "") {
+template<template<typename...> class Container>
+std::string joinStrings(const Container<std::string>& container, const std::string& sep = "") {
 	std::string ret;
-	for (auto it : vec) {
+	for (auto it : container) {
 		ret += it;
+	}
+	return ret;
+}
+
+std::string joinStrings(std::queue<std::string>& container, const std::string& sep = "") {
+	std::string ret;
+	while (!container.empty()) {
+		ret += container.front();
+		container.pop();
+		std::cout << "ret: " << ret << std::endl;
+	}
+	return ret;
+}
+
+std::string joinStrings(std::stack<std::string>& container, const std::string& sep = "") {
+	std::string ret;
+	while (!container.empty()) {
+		ret += container.top();
+		container.pop();
+		std::cout << "ret: " << ret << std::endl;
 	}
 	return ret;
 }
--- Conversions/tester.cpp
+++ Conversions/tester.cpp
@@ -6,6 +6,15 @@ int main() {
 	vec.push_back("hello ");
 	vec.push_back("Zorld ");
 	vec.push_back("World!");
-	std::string str = vectorToString(vec);
-	Printer::all(str);
+	std::string str = joinStrings(vec);
+	Printer::all("vector: ", str);
+
+	std::queue<std::string> q1;
+	q1.push("hello ");
+	q1.push("hello ");
+	q1.push("Zorld ");
+	q1.push("World!");
+	str = joinStrings(q1);
+
+	Printer::all("queue: ", str);
 }
