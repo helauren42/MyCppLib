2c85192f0e43722465047923878b6e9ac8ee55e1
helauren-laptop <helauren42@gmail.com>
2024-11-11 01:13:24 +0100

    split string to list working

--- Strings/Strings.hpp
+++ Strings/Strings.hpp
@@ -11,7 +11,7 @@ bool	isWhiteSpace(const char& c) {
 	const std::string white_spaces = WHITE_SPACES;
 
 	if(white_spaces.find(c) != std::string::npos)
-		return true;	
+		return true;
 	return false;
 }
 
@@ -66,4 +66,23 @@ std::string replaceAll(const std::string& str, const std::string& from, const st
 
 std::string removeAll(const std::string& str, const std::string& remove) {
 	return replaceAll(str, remove, "");
+}
+
+// template<typename T>
+std::list<std::string> split(const std::string& str, const std::string& delimiter = "\n") {
+	std::list<std::string> container;
+
+	size_t start = 0;
+	size_t end = 0;
+
+	while(true) {
+		if((start = str.find_first_not_of(delimiter, start)) == std::string::npos)
+			break;
+		end = str.find_first_of(delimiter, start);
+		if(end == std::string::npos)
+			end = str.length();
+		container.push_back(str.substr(start, end - start));
+		start = end+1;
+	}
+	return(container);
 }
--- Strings/tester.cpp
+++ Strings/tester.cpp
@@ -1,29 +1,34 @@
 #include "Strings.hpp"
 #include "../Printer/Printer.hpp"
 
-int main(void) {
-	std::string str = "  \n\tHello \nWorld!    \n   \n \t";
-	str = lstrip(str);
-	Printer::print(str);
-	str = rstrip(str);
-	Printer::print(str, "\n");
+// int main(void) {
+// 	std::string str = "  \n\tHello \nWorld!    \n   \n \t";
+// 	str = lstrip(str);
+// 	Printer::print(str);
+// 	str = rstrip(str);
+// 	Printer::print(str, "\n");
 
-	std::string str2 = "  \n\tHello \nWorld!    \n   \n \t";
-	str2 = strip(str2);
-	Printer::all("strip: ", str2);
+// 	std::string str2 = "  \n\tHello \nWorld!    \n   \n \t";
+// 	str2 = strip(str2);
+// 	Printer::all("strip: ", str2);
 
-	std::string str3 = "dogogdddgooddgogoodddddHello World! dogododdddogggodgo";
-	str3 = strip(str3, "dog");
-	Printer::all("strip 3: ", str3);
+// 	std::string str3 = "dogogdddgooddgogoodddddHello World! dogododdddogggodgo";
+// 	str3 = strip(str3, "dog");
+// 	Printer::all("strip 3: ", str3);
 
-	std::string og = "what day ewre day awq day!";
-	std::string from = "day";
-	std::string to = "night";
+// 	std::string og = "what day ewre day awq day!";
+// 	std::string from = "day";
+// 	std::string to = "night";
 
-	std::string ng = replaceAll(og, from, to);
+// 	std::string ng = replaceAll(og, from, to);
 
-	Printer::all(og);
-	Printer::all(ng);
-	std::string removed = removeAll(ng, "night");
-	Printer::all(removed);
+// 	Printer::all(og);
+// 	Printer::all(ng);
+// 	std::string removed = removeAll(ng, "night");
+// 	Printer::all(removed);
+// }
+
+int main() {
+	std::string og = "what day ewre day awq day!a";
+	Printer::all(split(og, "day"));
 }
