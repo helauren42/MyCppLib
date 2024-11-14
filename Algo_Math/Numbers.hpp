c73c3dbce8564aaa308fbe15f36c9aaa8e5e78e9
helauren-laptop <helauren42@gmail.com>
2024-11-14 22:22:42 +0100

    templated getPosition

--- Algo_Math/Numbers.hpp
+++ Algo_Math/Numbers.hpp
@@ -4,12 +4,12 @@
 #include <vector>
 #include <set>
 
-template<typename T>
-int	getPosition(const std::vector<T>& container, T value) {
+template<template<typename> class Container, typename T>
+int	getPosition(const Container<T>& container, const T& value) {
 	auto it = std::find(container.begin(), container.end(), value);
 	if(it == container.end())
 		return -1;
-	return *(std::distance(container.begin(), it));
+	return (std::distance(container.begin(), it));
 }
 
 
