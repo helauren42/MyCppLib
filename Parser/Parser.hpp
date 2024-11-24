afdfd32b7258647e60eb85a4dce21f8f3666b48d
helauren-laptop <helauren42@gmail.com>
2024-11-24 22:47:47 +0100

    check

--- Parser/Parser.hpp
+++ Parser/Parser.hpp
@@ -89,23 +89,7 @@ class Parser {
 		std::map <std::string, Argument*> option_args;
 		std::map <std::string, Argument*> positional_args;
 		std::map <std::string, Argument*>::iterator positional_args_it;
-	public:
-		static const int BOOLEAN = 0;
-		static const int INT = 1;
-		static const int STRING = 2;
- 		Parser() {
-			to_parse = {};
-			option_args = {};
-			positional_args = {};
-		};
-		~Parser() {
-			for (auto it = option_args.begin(); it != option_args.end(); it++) {
-				delete it->second;
-			}
-			for (auto it = positional_args.begin(); it != positional_args.end(); it++) {
-				delete it->second;
-			}
-		};
+
 		void	printArgs() {
 			for (auto it = option_args.begin(); it != option_args.end(); it++) {
 				std::cout << "deleting " << it->first << std::endl;
@@ -119,12 +103,6 @@ class Parser {
 		std::map <std::string, int> getToParse() const {
 			return to_parse;
 		}
-		std::map <std::string, Argument*> getArgs() const {
-			std::map<std::string, Argument*> merged;
-			merged.insert(option_args.begin(), option_args.end());
-			merged.insert(positional_args.begin(), positional_args.end());
-			return merged;
-		}
 		bool	isOption(std::string elem) const {
 			if(elem.find("--") != std::string::npos)
 				return true;
@@ -132,11 +110,6 @@ class Parser {
 				return true;
 			return false;
 		}
-		void	addArgument(std::string name, int type) {
-			if(type < BOOLEAN || type > STRING)
-				throw (std::invalid_argument("Use the class's inherent static attributes such as Parser::BOOLEAN, Parser::INT, Parser::STRING"));
-			to_parse[name] = type;
-		};
 		void	parseArg(std::string value) {
 			if(positional_args_it == positional_args.end())
 				throw (std::invalid_argument("Positional argument not added to parse " + value));
@@ -162,13 +135,43 @@ class Parser {
 				option_args[name] = new String(value);
 			return "";
 		}
+	public:
+		static const int BOOLEAN = 0;
+		static const int INT = 1;
+		static const int STRING = 2;
+ 		Parser() {
+			to_parse = {};
+			option_args = {};
+			positional_args = {};
+		};
+		~Parser() {
+			for (auto it = option_args.begin(); it != option_args.end(); it++) {
+				delete it->second;
+			}
+			for (auto it = positional_args.begin(); it != positional_args.end(); it++) {
+				delete it->second;
+			}
+		};
+		std::map <std::string, Argument*> getArgs() const {
+			std::map<std::string, Argument*> merged;
+			merged.insert(option_args.begin(), option_args.end());
+			merged.insert(positional_args.begin(), positional_args.end());
+			return merged;
+		}
+		void	addArgument(std::string name, int type) {
+			if(type < BOOLEAN || type > STRING)
+				throw (std::invalid_argument("Use the class's inherent static attributes such as Parser::BOOLEAN, Parser::INT, Parser::STRING"));
+			to_parse[name] = type;
+			if(!isOption(name))
+				positional_args[name] = nullptr;
+		};
 		void	parseArguments(char **av) {
 			std::string temp_option;
 			positional_args_it = positional_args.begin();
 			out("to_parse: ", to_parse);
 			for (int i = 1; av[i]; i++) {
 				std::string elem(av[i]);
-				out("elem: ",elem);
+				out("elem: ", elem);
 				if(isOption(elem)) {
 					temp_option = parseOption(elem);
 					continue;
