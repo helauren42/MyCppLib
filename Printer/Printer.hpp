ee844002476ea496692a1bdf5ba45a63865a5404
helauren-laptop <helauren42@gmail.com>
2025-02-24 16:43:52 +0100

    pre pull

--- Printer/Printer.hpp
+++ Printer/Printer.hpp
@@ -42,13 +42,11 @@ public:
 private:
 	template <typename T, template <typename...> class Template>
 	struct is_specialization : std::false_type
-	{
-	};
+	{};
 
 	template <template <typename...> class Template, typename... Args>
 	struct is_specialization<Template<Args...>, Template> : std::true_type
-	{
-	};
+	{};
 };
 
 #endif
